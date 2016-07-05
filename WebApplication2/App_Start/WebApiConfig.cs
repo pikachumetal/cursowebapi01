fa82296fe3e4c0bb86aa03455a552e92beaf8298
pikachumetal <pikachu.metal@gmail.com>
2016-07-05 13:50:07 +0200

    bla bla bla...

---
+++ WebApplication2/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApplication2
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApiV1",
+            //    routeTemplate: "api/v1/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+        }
+    }
+}
